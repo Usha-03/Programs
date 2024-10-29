0139d1ecee30bd8b73fcc1a0d8df2a38eb56ffed
Gottam Usha <110991830+Usha-03@users.noreply.github.com>
2024-10-29 14:58:19 +0530

    Add files via upload

---
+++ WinFormsAppCreate/WinFormsAppCreate/StoreProcedureMulParam.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WinFormsAppCreate
+{
+    partial class StoreProcedureMulParam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmpName = new TextBox();
+            lblEmpId = new Label();
+            btnInsert = new Button();
+            txtEmpId = new TextBox();
+            lblEmployeeId = new Label();
+            lblEmpDesignation = new Label();
+            txtEmpDesig = new TextBox();
+            lblEmpDOJ = new Label();
+            txtEmpDOJ = new TextBox();
+            txtEmpSal = new TextBox();
+            txtEmpDept = new TextBox();
+            lblEmpSal = new Label();
+            lblEmpDeptno = new Label();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            btnFind = new Button();
+            SuspendLayout();
+            // 
+            // txtEmpName
+            // 
+            txtEmpName.Location = new Point(272, 70);
+            txtEmpName.Name = "txtEmpName";
+            txtEmpName.Size = new Size(214, 27);
+            txtEmpName.TabIndex = 0;
+            // 
+            // lblEmpId
+            // 
+            lblEmpId.AutoSize = true;
+            lblEmpId.Location = new Point(112, 77);
+            lblEmpId.Name = "lblEmpId";
+            lblEmpId.Size = new Size(119, 20);
+            lblEmpId.TabIndex = 1;
+            lblEmpId.Text = "Employee Name";
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(159, 302);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(94, 29);
+            btnInsert.TabIndex = 2;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // txtEmpId
+            // 
+            txtEmpId.Location = new Point(272, 30);
+            txtEmpId.Name = "txtEmpId";
+            txtEmpId.Size = new Size(214, 27);
+            txtEmpId.TabIndex = 3;
+            // 
+            // lblEmployeeId
+            // 
+            lblEmployeeId.AutoSize = true;
+            lblEmployeeId.Location = new Point(112, 37);
+            lblEmployeeId.Name = "lblEmployeeId";
+            lblEmployeeId.Size = new Size(52, 20);
+            lblEmployeeId.TabIndex = 4;
+            lblEmployeeId.Text = "EmpId";
+            // 
+            // lblEmpDesignation
+            // 
+            lblEmpDesignation.AutoSize = true;
+            lblEmpDesignation.Location = new Point(112, 115);
+            lblEmpDesignation.Name = "lblEmpDesignation";
+            lblEmpDesignation.Size = new Size(123, 20);
+            lblEmpDesignation.TabIndex = 5;
+            lblEmpDesignation.Text = "Emp Designation";
+            // 
+            // txtEmpDesig
+            // 
+            txtEmpDesig.Location = new Point(272, 115);
+            txtEmpDesig.Name = "txtEmpDesig";
+            txtEmpDesig.Size = new Size(214, 27);
+            txtEmpDesig.TabIndex = 6;
+            // 
+            // lblEmpDOJ
+            // 
+            lblEmpDOJ.AutoSize = true;
+            lblEmpDOJ.Location = new Point(112, 151);
+            lblEmpDOJ.Name = "lblEmpDOJ";
+            lblEmpDOJ.Size = new Size(70, 20);
+            lblEmpDOJ.TabIndex = 7;
+            lblEmpDOJ.Text = "Emp DOJ";
+            // 
+            // txtEmpDOJ
+            // 
+            txtEmpDOJ.Location = new Point(272, 151);
+            txtEmpDOJ.Name = "txtEmpDOJ";
+            txtEmpDOJ.Size = new Size(214, 27);
+            txtEmpDOJ.TabIndex = 8;
+            // 
+            // txtEmpSal
+            // 
+            txtEmpSal.Location = new Point(272, 194);
+            txtEmpSal.Name = "txtEmpSal";
+            txtEmpSal.Size = new Size(214, 27);
+            txtEmpSal.TabIndex = 9;
+            // 
+            // txtEmpDept
+            // 
+            txtEmpDept.Location = new Point(272, 233);
+            txtEmpDept.Name = "txtEmpDept";
+            txtEmpDept.Size = new Size(214, 27);
+            txtEmpDept.TabIndex = 10;
+            // 
+            // lblEmpSal
+            // 
+            lblEmpSal.AutoSize = true;
+            lblEmpSal.Location = new Point(112, 194);
+            lblEmpSal.Name = "lblEmpSal";
+            lblEmpSal.Size = new Size(63, 20);
+            lblEmpSal.TabIndex = 11;
+            lblEmpSal.Text = "Emp Sal";
+            // 
+            // lblEmpDeptno
+            // 
+            lblEmpDeptno.AutoSize = true;
+            lblEmpDeptno.Location = new Point(112, 233);
+            lblEmpDeptno.Name = "lblEmpDeptno";
+            lblEmpDeptno.Size = new Size(93, 20);
+            lblEmpDeptno.TabIndex = 12;
+            lblEmpDeptno.Text = "Emp Deptno";
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(272, 302);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 13;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(159, 337);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 14;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(272, 337);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 15;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnFind
+            // 
+            btnFind.Location = new Point(434, 321);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(94, 29);
+            btnFind.TabIndex = 16;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = true;
+            btnFind.Click += btnFind_Click;
+            // 
+            // StoreProcedureMulParam
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnFind);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(lblEmpDeptno);
+            Controls.Add(lblEmpSal);
+            Controls.Add(txtEmpDept);
+            Controls.Add(txtEmpSal);
+            Controls.Add(txtEmpDOJ);
+            Controls.Add(lblEmpDOJ);
+            Controls.Add(txtEmpDesig);
+            Controls.Add(lblEmpDesignation);
+            Controls.Add(lblEmployeeId);
+            Controls.Add(txtEmpId);
+            Controls.Add(btnInsert);
+            Controls.Add(lblEmpId);
+            Controls.Add(txtEmpName);
+            Name = "StoreProcedureMulParam";
+            Text = "StoreProcedureMulParam";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmpName;
+        private Label lblEmpId;
+        private Button btnInsert;
+        private TextBox txtEmpId;
+        private Label lblEmployeeId;
+        private Label lblEmpDesignation;
+        private TextBox txtEmpDesig;
+        private Label lblEmpDOJ;
+        private TextBox txtEmpDOJ;
+        private TextBox txtEmpSal;
+        private TextBox txtEmpDept;
+        private Label lblEmpSal;
+        private Label lblEmpDeptno;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btnFind;
+    }
+}
---
+++ WinFormsAppCreate/WinFormsAppCreate/StoreProcedureMulParam.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WinFormsAppCreate
+{
+    public partial class StoreProcedureMulParam : Form
+    {
+        SqlConnection con = new SqlConnection("Server=USHA\\USHA;Database=EmpDB;User Id=sa;Password=user123;");
+        SqlCommand cmd;
+        SqlParameter p1;
+       // SqlDataReader dr;
+        public StoreProcedureMulParam()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("SPEmp_Update", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            p1 = new SqlParameter("@Pempid", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpId.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempname", SqlDbType.VarChar);
+            p1.Value = txtEmpName.Text;
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempdesig", SqlDbType.VarChar);
+            p1.Value = txtEmpDesig.Text;
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempdoj", SqlDbType.DateTime);
+            p1.Value = Convert.ToDateTime(txtEmpDOJ.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempsal", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpSal.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@PempDept", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpDept.Text);
+            cmd.Parameters.Add(p1);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(i + "record(s) Updated");
+        }
+
+        //private void lblEmployeeId_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void txtEmployeeId_TextChanged(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("SPEmp_Insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            p1 = new SqlParameter("@Pempid", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpId.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempname", SqlDbType.VarChar);
+            p1.Value = txtEmpName.Text;
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempdesig", SqlDbType.VarChar);
+            p1.Value = txtEmpDesig.Text;
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempdoj", SqlDbType.DateTime);
+            p1.Value = Convert.ToDateTime(txtEmpDOJ.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempsal", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpSal.Text);
+            cmd.Parameters.Add(p1);
+
+            p1 = new SqlParameter("@Pempdept", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpDept.Text);
+            cmd.Parameters.Add(p1);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(i + "record(s) Inserted");
+
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("SPEmp_Del", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            p1 = new SqlParameter("@Pempid", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpId.Text);
+            cmd.Parameters.Add(p1);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show(i + "record(S) deleted");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+                if (ctrl is TextBox)
+                {
+                    ctrl.Text = "";
+                }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            /*cmd = new SqlCommand("select * from EmployeeTB where EmpId = " + txtEmpId.Text, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read() == true)
+            {
+                txtEmpId.Text = dr[0].ToString();
+                txtEmpName.Text = dr[1].ToString();
+                txtEmpDesig.Text = dr[2].ToString();
+                txtEmpDOJ.Text = dr[3].ToString();
+                txtEmpSal.Text = dr[4].ToString();
+                txtEmpDept.Text = dr[5].ToString();
+            }
+            else
+            {
+                MessageBox.Show("record(s) not found");
+                dr.Close();
+            }
+            con.Close();*/
+            SqlCommand cmd = new SqlCommand("SPEmp_Find", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter p1 = new SqlParameter("@Pempid", SqlDbType.Int);
+            p1.Value = Convert.ToInt32(txtEmpId.Text);
+            cmd.Parameters.Add(p1);
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                txtEmpId.Text = reader[0].ToString();
+                txtEmpName.Text = reader[1].ToString();
+                txtEmpDesig.Text = reader[2].ToString();
+                txtEmpDOJ.Text = reader[3].ToString();
+                txtEmpSal.Text = reader[4].ToString();
+                txtEmpDept.Text = reader[5].ToString();
+            }
+            else
+            {
+                MessageBox.Show("record(s) not found");
+            }
+            con.Close();
+
+        }
+    }
+}
